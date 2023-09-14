8dca3ec692edc3852b31dcbba11ac61c50d55108
tonyprinz-ufes <antonio.v.moraes@edu.ufes.br>
2023-09-14 14:58:27 -0300

    atualiza 1

--- 04_TAD_simples/TAD_04/Respostas/antonio/aluno.h
+++ 04_TAD_simples/TAD_04/Respostas/antonio/aluno.h
@@ -1,5 +1,11 @@
-#include "aluno.h"
-#include <stdio.h>
+#ifndef _ALUNO_H_
+#define _ALUNO_H_
+
+typedef struct Aluno {
+    char nome[50]; /**< Nome do aluno */
+    int matricula; /**< Matrícula do aluno */
+    int n1, n2, n3; /**< Notas das avaliações */
+} tAluno;
 
 /**
  * @brief Cria um novo aluno com os dados informados.
@@ -11,24 +17,14 @@
  * @param n3 Nota da terceira avaliação.
  * @return tAluno Retorna uma estrutura tAluno com os dados do aluno criado.
  */
-tAluno criar_aluno(char *nome, int matricula, int n1, int n2, int n3){
-    // scanf("%s" aluno.nome);
-    // scanf("%d" &aluno.matricula);
-    // scanf("%d %d %d" &aluno.n1, &aluno.n2, &aluno.n3);
-    tAluno aluno;
-    int i = 0;
-    while (nome[i]!="\0")
-    {
-        aluno.nome[i] = nome[0];
-        i = i + 1;
-    };
-    aluno.matricula = matricula;
-    aluno.n1 = n1;
-    aluno.n2 = n2;
-    aluno.n3 = n3;
-    return aluno;
-    
-};
+tAluno CriaAluno(char *nome, int matricula, int n1, int n2, int n3); 
+
+/**
+ * @brief Lê os dados de um aluno.
+ * 
+ * @return tAluno Retorna uma estrutura tAluno com os dados do aluno lido.
+ */
+tAluno LeAluno();
 
 /**
  * @brief Compara as matrículas de dois alunos.
@@ -37,15 +33,7 @@ tAluno criar_aluno(char *nome, int matricula, int n1, int n2, int n3){
  * @param aluno2 Segundo aluno a ser comparado.
  * @return int Retorna 1 se a matrícula do aluno1 é maior que a do aluno2, -1 se a matrícula do aluno1 é menor que a do aluno2 e 0 se as matrículas são iguais.
  */
-int comparar_matricula_aluno(tAluno aluno1, tAluno aluno2){
-    if (aluno1.matricula > aluno2.matricula){
-        return 1;
-    };
-    if (aluno1.matricula < aluno2.matricula){
-        return -1;
-    };
-    return 0;
-};
+int ComparaMatricula(tAluno aluno1, tAluno aluno2);
 
 /**
  * @brief Calcula a média das notas de um aluno.
@@ -53,10 +41,7 @@ int comparar_matricula_aluno(tAluno aluno1, tAluno aluno2){
  * @param aluno Aluno a ter a média calculada.
  * @return int Retorna a média das notas do aluno.
  */
-int calcular_media_aluno(tAluno aluno){
-    int media = ((aluno.n1)+(aluno.n2)+(aluno.n3))/3;
-    return media;
-};
+int CalculaMediaAluno(tAluno aluno);
 
 /**
  * @brief Verifica se um aluno foi aprovado ou reprovado.
@@ -64,19 +49,13 @@ int calcular_media_aluno(tAluno aluno){
  * @param aluno Aluno a ser verificado.
  * @return int Retorna 1 se o aluno foi aprovado e 0 se foi reprovado.
  */
-int aluno_aprovado(tAluno aluno){
-    float media = ((aluno.n1)+(aluno.n2)+(aluno.n3))/3.0;
-    if (media >=7.0){
-        return 1;
-    };
-    return 0;
-};
+int VerificaAprovacao(tAluno aluno);
 
 /**
  * @brief Imprime os dados de um aluno.
  * 
  * @param aluno Aluno a ser impresso.
  */
-void imprimir_aluno(tAluno aluno){
-    printf("%s", aluno.nome);
-};
+void ImprimeAluno(tAluno aluno);
+
+#endif
--- 04_TAD_simples/TAD_04/Respostas/antonio/main.c
+++ 04_TAD_simples/TAD_04/Respostas/antonio/main.c
@@ -4,6 +4,19 @@
 int main (){
     int contador;
     scanf ("%d", &contador);
-    if ()
+    int i = 0;
+    char nome[50];
+    int matricula; 
+    int n1, n2, n3;
+    tAluno vetor[contador-1];
+    while (i<=contador)
+    {
+    scanf("%s", nome);
+    scanf("%d", &matricula);
+    scanf("%d %d %d", &n1, &n2, &n3);
+    vetor[i] = CriaAluno ( nome, matricula, n1, n2, n3);
+    i ++;
+    };
+    
     return 0;
 }
